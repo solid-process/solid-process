98b274add7166ce4b70eba973c999dd7c48019ae
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-04-17 07:42:35 -0300

    Refactor instance_of_validator

--- lib/solid/validators/instance_of_validator.rb
+++ lib/solid/validators/instance_of_validator.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require "solid/validators"
+
 class InstanceOfValidator < ActiveModel::EachValidator
-  def validate_each(obj, attribute, value)
+  def validate_each(model, attribute, value)
     with_option = Array.wrap(options[:with] || options[:in])
 
     return if with_option.any? { |type| value.instance_of?(type) }
 
-    expectation = with_option.map(&:name).join(" | ")
+    message = "is not an instance of #{with_option.map(&:name).join(" | ")}"
 
-    obj.errors.add(attribute, (options[:message] || "is not an instance of #{expectation}"))
+    Solid::Validators.add_error(model, attribute, message, options)
   end
 end
